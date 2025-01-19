a74064e0570730a9903565e3df716c2389b1b628
euhansarkar <euhansarkar@gmail.com>
2025-01-20 05:28:11 +0600

    Added: Zod Validation In Schedule And Booking Schema.

--- src/app/modules/booking/booking.validation.ts
+++ src/app/modules/booking/booking.validation.ts
@@ -1,21 +1,37 @@
 import { z } from "zod";
 
 const create = z.object({
-    body: z.object({
-        name: z.string({
-            required_error: "name is required",
-        }),
-        image: z.string({
-            required_error: "image is required",
-        })
+  scheduleId: z.string({
+    required_error: 'Schedule ID is required.',
+    invalid_type_error: 'Schedule ID must be a valid string.',
+  }),
+  traineeId: z.string({
+    required_error: 'Trainee ID is required.',
+    invalid_type_error: 'Trainee ID must be a valid string.',
+  }),
+  isCancelled: z
+    .boolean({
+      invalid_type_error: 'isCancelled must be a boolean value.',
     })
-})
+    .optional(),
+});
 
 const update = z.object({
-    body: z.object({
-        name: z.string(),
-        image: z.string().optional()
+  scheduleId: z
+    .string({
+      invalid_type_error: 'Schedule ID must be a valid string.',
     })
+    .optional(),
+  traineeId: z
+    .string({
+      invalid_type_error: 'Trainee ID must be a valid string.',
+    })
+    .optional(),
+  isCancelled: z
+    .boolean({
+      invalid_type_error: 'isCancelled must be a boolean value.',
+    })
+    .optional(),
 });
 
 export const BookingValidation = {
